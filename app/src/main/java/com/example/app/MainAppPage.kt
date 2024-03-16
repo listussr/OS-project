a7b403834f28909674c60a01d90c6ceef8b10799
Vsevolod <listussr85@gmail.com>
2024-03-16 15:15:41 +0400

    Fixed second page

--- app/src/main/java/com/example/app/MainAppPage.kt
+++ app/src/main/java/com/example/app/MainAppPage.kt
@@ -2,8 +2,9 @@ package com.example.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 
-class MainAppPage : AppCompatActivity() {
+class MainAppPage : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_app_page)
